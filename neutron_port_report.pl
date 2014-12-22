e162ec4a4499d19989172c4a70c71db49df318f2
jmcdice <jmcdice@gmail.com>
2014-12-22 19:54:07 +0200

    Need to account for the DHCP interface for each network.

--- neutron_port_report.pl
+++ neutron_port_report.pl
@@ -5,7 +5,7 @@
 
 use strict;
 my @ports = `neutron port-list -c id -c fixed_ips`;
-my (@ips, @mess, @orphans, %totals, $port_total);
+my (@ips, @mess, @orphans, %totals, $port_total, $nets);
 
 for my $ip (`neutron port-list -c fixed_ips|grep subnet | awk '{print \$5}'`) {
    chomp $ip;
@@ -33,7 +33,15 @@ for my $ip (@ips) {
    }
 }
 
+# There is one dhcp interface in neutron which we need to subtract from each network.
+for ( `neutron net-list` ) {
+   next unless /[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}/;
+   $nets++
+}
+   
 my $count = scalar @orphans;
+$count = $count - $nets;
+
 print "There are $count orphaned neutron ports from a total of $port_total ports.\n";
 
 sub uniq {
