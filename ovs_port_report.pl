21547915db9f17627a50bfbfaba16d95ea670138
jmcdice <jmcdice@gmail.com>
2014-12-22 20:26:46 +0200

    This script will detect ports created by L3 Agent which should have been cleaned up already.

---
+++ ovs_port_report.pl
@@ -0,0 +1,54 @@
+#!/usr/bin/perl
+#
+# Look in OVS and see if we have any orphaned ports.
+# Defined as, ports configured in OVS by neutron, that no longer exist in neutron.
+# Run me from a rocks frontend.
+# Joey <jmcdice@gmail.com>
+# 
+
+use strict;
+
+my %aports; # Active ports in Neutron
+
+for ( get_neutron_ports() ) {
+   $aports{$_}++
+}
+
+for my $dport ( get_deployed_ports() ) {
+   print "Unassociated port: $dport\n" unless ( $aports{$dport} )
+}
+
+sub get_deployed_ports() {
+
+   my @ports;
+   for ( `rocks run host compute 'ovs-vsctl show|grep Port | grep qvo'` ) {
+      push @ports, $1 if (/(qvo.*?)\"/);
+   }
+   return @ports;
+}
+
+sub get_neutron_ports() {
+
+   my @neutron_ports = `ssh os-controller 'neutron port-list'`;
+   my @ovs_ports;
+
+   for (@neutron_ports) {
+      my $qvo = $1 if /([0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12})/;
+      next unless $qvo; 
+   
+      # If this port is bound to os-network, then it's the DHCP interface and we should ignore it.
+      my $dhcp_if;
+      for ( `ssh os-controller 'neutron port-show $qvo'` ) {
+         $dhcp_if++ if /os-network.local/
+      }
+      next if ( $dhcp_if );
+   
+      # neutron L3 agent creates ports in OVS, like this:
+      # c0a830ee-f132-4ca4-ac18-b3f8c8219211 = 'qvoc0a830ee-f1'
+      my $port = "qvo" . substr( $qvo, 0, 11 );
+   
+      # Return a list of ports we need to investigate.
+      push @ovs_ports, $port;
+   }
+   return @ovs_ports;
+}
