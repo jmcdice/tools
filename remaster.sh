db624b2bbb447e741c24960273f75f65d25d4e27
Joey <joseph.mcdonald@alcatel-lucent.com>
2015-02-03 01:13:53 +0000

    Added some output clean up.

--- remaster.sh
+++ remaster.sh
@@ -12,13 +12,15 @@ fi
 
 function prep_workspace() {
 
-   echo "Preparing work space."
+   echo -n "Preparing work space: "
    rm -rf /export/build_iso/x86_64/
    mkdir -p /export/build_iso/x86_64/
-   mount -o loop /export/rocks-2.4/iso/ALU-7.0.1.x86_64.disk1.iso /mnt/cdrom/
-   echo "Copying in original ISO."
+   mount -o loop /export/iso/ALU-7.0.1.x86_64.disk1.iso /mnt/cdrom/
+   echo "Ok"
+
+   echo -n "Copying in original ISO: "
    rsync -a /mnt/cdrom/ /export/build_iso/x86_64/ 
-   umount /mnt/cdrom/
+   echo "Ok"
 }
 
 function make_initrd() {
@@ -28,24 +30,28 @@ function make_initrd() {
    mkdir initrd
    cd initrd
    xz -dc /mnt/cdrom/isolinux/initrd.img | cpio -id &> /dev/null
+   umount /mnt/cdrom/
 
    # Greg's site.attrs
    cp /root/stackiq/site.attrs.stackiq /export/build_iso/x86_64/initrd/tmp/site.attrs
    cp /root/stackiq/rolls.xml /export/build_iso/x86_64/initrd/tmp/rolls.xml
    
-   cd /export/build_iso/x86_64/initrd && find . | cpio --quiet -c -o | xz --verbose -0 --format=lzma > ../isolinux/initrd.img 
+   # Repack
+   cd /export/build_iso/x86_64/initrd && find . | cpio --quiet -c -o | xz --verbose -0 --format=lzma > ../isolinux/initrd.img  &> /dev/null
    rm -rf initrd/
+   echo "Ok"
 }
 
 function make_iso() {
 
-   echo "Creating ISO file"
+   echo -n "Creating ISO file: "
    mkdir -p /var/www/html/iso/auto/$cluster/
-   output="/var/www/html/iso/auto/$cluster/$cluster-fe-install.iso";
+   output="/export/iso/$cluster-fe-install.iso";
    cmd="mkisofs -V 'ALU' -b isolinux/isolinux.bin -c isolinux/boot.cat -no-emul-boot -boot-load-size 6 -boot-info-table -r -T -f -input-charset utf-8 -m initrd -m alu-fe -o $output ." 
 
-   cd /export/build_iso/x86_64 
-   $cmd 
+   cd /export/build_iso/x86_64/
+   $cmd &> /dev/null 
+   du -sh $output 
 }
 
 echo "Customizing Rocks ISO for: $cluster"
