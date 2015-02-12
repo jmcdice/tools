a1e861d080f0e8bb105f6568203b1d2eeb8cf0d7
jmcdice <jmcdice@gmail.com>
2015-02-12 06:57:15 +0000

    Needed to umount /dev/cdrom/

--- remaster.sh
+++ remaster.sh
@@ -12,8 +12,8 @@ fi
 
 function prep_workspace() {
 
+   # Copy the contents of the StackIQ ISO to disk
    echo -n "Preparing work space: "
-
    test -d /mnt/cdrom || mkdir -p /mnt/cdrom/
    test -d /mnt/liveos || mkdir -p /mnt/liveos/
 
@@ -24,21 +24,22 @@ function prep_workspace() {
 
    echo -n "Copying in original ISO: "
    rsync -a /mnt/cdrom/ /export/build_iso/x86_64/ 
+   umount /mnt/cdrom/
    echo "Ok"
 }
 
 function make_squashfs() {
 
+   # Extract the file system in squashfs.img to disk
    echo -n "Preparing squashfs filesystem: "
    cd /export/build_iso/x86_64/
    rpm -q squashfs-tools &> /dev/null || yum -y install squashfs-tools
-
    cd /export/build_iso/x86_64/
-
    mount LiveOS/squashfs.img /mnt/cdrom/ -o loop -t squashfs
    mkdir squashfs
    rsync -a /mnt/cdrom/ squashfs/
    umount /mnt/cdrom/
+   rm LiveOS/squashfs.img
 
    mkdir rootfs
    mount squashfs/LiveOS/rootfs.img rootfs/ -o loop
@@ -47,9 +48,8 @@ function make_squashfs() {
    cp /root/stackiq/site.attrs.auto rootfs/tmp/site.attrs
    cp /root/stackiq/rolls.xml rootfs/tmp/rolls.xml
 
-   rm LiveOS/squashfs.img
-   mksquashfs /export/build_iso/x86_64/rootfs /export/build_iso/x86_64/LiveOS/squashfs.img
-
+   # Rebundle file system into squashfs.img
+   mksquashfs /export/build_iso/x86_64/rootfs /export/build_iso/x86_64/LiveOS/squashfs.img -keep-as-directory
    umount /export/build_iso/x86_64/rootfs/
    rm -rf rootfs squashfs
    umount /mnt/cdrom/
