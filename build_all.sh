0e6e9675f7629ce243c6e52c5a288c1279fbeaab
Your Name <you@example.com>
2016-07-19 16:48:43 -0400

    Minor updates.

--- build_all.sh
+++ build_all.sh
@@ -1,34 +1,59 @@
+#!/usr/bin/bash
+#
+# Create the overcloud images with our customizations and deploy the overcloud.
+#
+# Yoshi <yossi.ovadia@nokia.com>
+# Joy!  <joey.mcdonald@nokia.com>
+
 set -x
 
 source /home/stack/temp_images/my_export
 
 function build_images() {
 
+   echo -n "Cleaning up old image files: "
    cd ~/temp_images \
       sudo cd /home/stack/temp_images; \
       rm -rf deploy-ramdisk-ironic.* dib* \
       ironic-python-agent.* overcloud-full.* 
       *.log fedora-user.qcow2
+   echo "Ok"
+
+   # Time saver if you need only overcloud-full
+   # openstack overcloud image build --type overcloud-full
 
    time openstack overcloud image build --all \
       --builder-extra-args cbis 2>&1 | tee openstack_image_build.log
 
+   echo -n "Copying images into place: "
    sudo cp ironic-python-agent.initramfs ~/images/ironic-python-agent.initramfs
    sudo cp ironic-python-agent.kernel ~/images/ironic-python-agent.kernel
    sudo cp overcloud-full.* ~/images/
+   echo "Ok"
 }
 
 function upload_images() {
+
+   echo -n "Uploading images to glance: "
    . ~/stackrc; 
    cd /home/stack/images && openstack overcloud image upload --update-existing
-   openstack baremetal configure boot 
+   echo "Ok"
 }
 
 function nova_boot() {
+
+   # If you rebuild the ramdisk, Ironic needs to know about it, so just always run it.
+   openstack baremetal configure boot
+   sleep 3
+
    imageid=$(glance image-list|grep 'overcloud-full '|awk '{print $2}')
    networkid=$(neutron net-list|grep ctlplane|awk '{print $2}')
-   nova_boot="nova boot --image=$imageid --flavor=baremetal --key admin-key --nic net-id=$networkid install-test-01"
+   nova_boot="nova boot --image=$imageid --flavor=baremetal --key sun-key --nic net-id=$networkid install-test-01"
+
    $nova_boot
+   sleep 5
+   ipmi=$(ironic node-show $(ironic node-list |egrep 'wait|deploy' | awk '{print $2}')|perl -lane 'print $1 if (/ipmi_address.*?u'\''(.*?)'\''/)')
+   echo "Deploying on IPMI: $ipmi"
 }
 
 function delete_all_glance_images() {
@@ -37,6 +62,9 @@ function delete_all_glance_images() {
 }
 
 function stack_delete() {
+
+   nova list | grep install-test-01 | perl -lane 'system "nova delete @F[1]"'
+
    heat stack-list | grep -q overcloud
    if [ $? == 0 ]; then
       echo 'y' | heat stack-delete overcloud
@@ -44,12 +72,12 @@ function stack_delete() {
       echo -n "Waiting for stack-delete: "
       for i in {1..30}; do
 
+         # If we get here, stack-delete again and it should work
          heat stack-list|grep -q "DELETE_FAILED"
          if [ $? == 0 ]; then
             echo 'y' | heat stack-delete overcloud
             sleep 3
          fi
-
 
          heat stack-list|grep -q overcloud
          if [ $? == 0 ]; then
@@ -67,20 +95,27 @@ function stack_delete() {
 }
 
 function delete_ironic_nodes() {
-   ironic node-list | perl -lane 'system "ironic node-set-provision-state @F[1] delete"'
+
+   echo -n "Deleting existing nodes in ironic: "
+   ironic node-list | grep False | perl -lane 'system "ironic node-set-provision-state @F[1] deleted &> /dev/null"'
    ironic node-list | awk '/False/ { system ("ironic node-delete "$2) }'
+   echo "Ok"
 }
 
 function start_introspection() {
 
    # introspection
    openstack baremetal import --json ~/instackenv.json
-   openstack baremetal configure boot
    openstack baremetal introspection bulk start
+
+   # Trying to nova boot here returns an error in some cases, not sure why yet.
+   sleep 10
+   openstack baremetal configure boot
 }
 
 
 function overcloud_deploy() {
+
 
    cd ~/templates && openstack overcloud deploy --templates --ntp-server 10.39.255.232 \
          --control-scale 1 --compute-scale 3 --neutron-tunnel-types vxlan \
@@ -91,7 +126,7 @@ stack_delete
 delete_all_glance_images
 delete_ironic_nodes
 build_images
-start_introspection
 upload_images
+start_introspection
 nova_boot
 # overcloud_deploy
